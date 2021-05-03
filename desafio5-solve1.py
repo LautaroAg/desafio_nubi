8a9624937f12f8e9f5c3ca4229de37fdfd0cd2f1
LautaroAg <lautarocanales@gmail.com>
2021-05-03 20:36:07 -0300

    resuelve desafio 5

---
+++ desafio5-solve1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sys import exit
+
+def desafio5Solve1(parquet_file='./joined.parquet'):
+    df = pd.read_parquet(parquet_file, engine='auto')
+    new_column = []
+    for row in df.iterrows():
+        new_column.append(row[1]['date']+'-'+row[1]['category_id'])
+    df['row_key'] = new_column
+    df.columns = [str(col) for col in df.columns] # pasa los nombres a String para poder guardar como .parquet
+    df.to_parquet('./joinedWithRowKey.parquet', engine='auto')
+
+desafio5Solve1()
