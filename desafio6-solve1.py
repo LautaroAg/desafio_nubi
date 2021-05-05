76abdba0ae125a8c00f6ea6361db087d2a23a0f4
LautaroAg <lautarocanales@gmail.com>
2021-05-04 21:17:03 -0300

    CREO que esta funcionando, faltan testeos y, claramente, optimizacion

--- desafio6-solve1.py
+++ desafio6-solve1.py
@@ -5,21 +5,23 @@ def desafio6Solve1(parquet_file='./joinedWithRowKey.parquet'):
     """
     Resuelve el desafio 6
     """
-    dict_new = {}
+    data_dict = {}
     df = pd.read_parquet(parquet_file, engine='auto')
     for i in range(1,8):
         for row in df.iterrows():
             level = 'level' + str(i)
             level_id = str(row[1][level])
             try:
-                dict_new[(level_id,level)] += row[1]['sales']
+                data_dict[(level,level_id)] += row[1]['sales']
             except:
-                dict_new[(level_id,level)] = 0
-    
-
-    #data = [(key[1] , key[0], dict_new[(key[0],key[1])]) for key in dict_new.keys()]
-    #print(data)
-    df_new = pd.DataFrame(data, columns=['level', 'level_id', 'sales'])
+                data_dict[(level,level_id)] = 0
+    data_list = {'level':[],'level_id':[],'sales':[]}
+    for data_key in data_dict.keys():
+        data_list['level'].append(data_key[0])
+        data_list['level_id'].append(data_key[1])
+        data_list['sales'].append(data_dict[data_key])
+    print(data_list)
+    df_new = pd.DataFrame(data_list)
     df_new.to_parquet('./levels.parquet')
     
 desafio6Solve1()
