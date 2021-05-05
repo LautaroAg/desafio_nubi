fa30f0168e69f6f497052827b405aa1e13bd5c4e
LautaroAg <lautarocanales@gmail.com>
2021-05-05 20:43:19 -0300

    resuelve el desafio 7

---
+++ desafio7-solve1.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from sys import exit
+def generateMonthlyPathList(year, month, day, url='https://importantdata@location.com.ar/'):
+    """
+    Devuelve una lista con todas las url de la forma url/year/month/days
+    con todos los dias desde el primero hasta el pasado como parametro
+    """
+    if url[-1] != '/': url = f'{url}/'
+    try:
+        datetime.strptime(f'{year}/{month}/{day}', '%Y/%m/%d')
+    except:
+        exit('Incorrect date values')
+    return [f'{url}{year}/{month}/{each_day}/' for each_day in range(1,day+1)]
+
+
+[print(f'{e}\n') for e in (generateMonthlyPathList(2020,2,29))]
