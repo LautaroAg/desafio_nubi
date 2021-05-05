999067e4075fc1be344128f4e6f60c4f6089003e
Lautaro <lautarocanales@gmail.com>
2021-05-05 20:50:00 -0300

    refactoring cambiando un hardcodeo a un parametro

--- desafio5-solve1.py
+++ desafio5-solve1.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-def desafio5Solve1(parquet_file='./joined.parquet'):
+def desafio5Solve1(parquet_file='./joined.parquet', output='./joinedWithRowKey.parquet'):
     """
-    Resuelve el desafio 5 guardando el resultado en el archivo
-    'joinedWithRowKey.parquet'
+    Resuelve el desafio 5 guardando el resultado en el archivo recibido como parametro
+    o el default: 'joinedWithRowKey.parquet'
     """
     
     df = pd.read_parquet(parquet_file, engine='auto')
@@ -12,6 +12,6 @@ def desafio5Solve1(parquet_file='./joined.parquet'):
         new_column.append(row[1]['date']+'-'+row[1]['category_id'])
     df['row_key'] = new_column
     df.columns = [str(col) for col in df.columns] # pasa los nombres a String para poder guardar como .parquet
-    df.to_parquet('./joinedWithRowKey.parquet', engine='auto')
+    df.to_parquet(output, engine='auto')
 
 desafio5Solve1()
