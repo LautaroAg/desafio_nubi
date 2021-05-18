bbd616ad20a87fd05c377484cfb474f5672da0c4
Lautaro <lautarocanales@gmail.com>
2021-05-18 19:36:39 -0300

    [FIX] prefijos 0 y no rompe al recibir strings
    Ahora devuelve siempre la fecha con el prefijo 0 como se esperaba y funciona tanto para Strings como para Integers.

--- desafio7-solve1.py
+++ desafio7-solve1.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from sys import exit
+
+
 def generateMonthlyPathList(year, month, day, url='https://importantdata@location.com.ar/'):
     """
     Devuelve una lista con todas las url de la forma url/year/month/days
@@ -10,7 +12,10 @@ def generateMonthlyPathList(year, month, day, url='https://importantdata@locatio
         datetime.strptime(f'{year}/{month}/{day}', '%Y/%m/%d')
     except:
         exit('Incorrect date values')
-    return [f'{url}{year}/{month}/{each_day}/' for each_day in range(1,day+1)]
+    return [
+        f'{url}{"{:4d}/{:02d}/{:02d}/".format(int(year),int(month),int(each_day))}'
+        for each_day in range(1,int(day)+1) 
+    ]
 
 
-[print(f'{e}\n') for e in (generateMonthlyPathList(2020,2,29))]
+[ print(e) for e in (generateMonthlyPathList("2021","05","17")) ]
